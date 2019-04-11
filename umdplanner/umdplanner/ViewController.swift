ba96b469f24ff10f3a881b41e92c1d5e5950ad99
Robert Choe <Robert.d.choe67@gmail.com>
2019-04-11 15:04:28 -0400

    first commit

---
+++ umdplanner/umdplanner/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  umdplanner
+//
+//  Created by Robert Choe on 4/11/19.
+//  Copyright © 2019 group42. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
