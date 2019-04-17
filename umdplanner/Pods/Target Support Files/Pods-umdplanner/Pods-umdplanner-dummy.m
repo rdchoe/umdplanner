fb820299947a24d236c177d7d5f81fabdac47505
Robert Choe <Robert.d.choe67@gmail.com>
2019-04-16 20:33:13 -0400

    set up cocoapods framework

---
+++ umdplanner/Pods/Target Support Files/Pods-umdplanner/Pods-umdplanner-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_umdplanner : NSObject
+@end
+@implementation PodsDummy_Pods_umdplanner
+@end
---
+++ umdplanner/Pods/Target Support Files/Pods-umdplannerTests/Pods-umdplannerTests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_umdplannerTests : NSObject
+@end
+@implementation PodsDummy_Pods_umdplannerTests
+@end
